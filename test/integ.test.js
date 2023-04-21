34ed0eef3e514333e2e974171f25da63aa3b6dfa
Pierre <pierre_kengne@yahoo.com>
2023-04-20 23:01:32 -0400

    test

--- test/integ.test.js
+++ test/integ.test.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-test( "Service returns 'Hello World'", async() => {
+test( "Service returns 'Hello World!'", async() => {
     try {
         const response = await axios.get(process.env.SERVICE_ENDPOINT, {});
         expect(response.data).toBe("Hello world")
