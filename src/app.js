9569e4b95951040819557c52d4b9de3572d423c7
Pierre <pierre_kengne@yahoo.com>
2023-04-22 11:13:35 -0400

    comment

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const res = require("express/lib/response");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send('Hello World!');
 })
 
 const port = 8080
