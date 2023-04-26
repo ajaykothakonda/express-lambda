49e7ac7715d8baaa91ab5e56a74a2ba511084248
Pierre <pierre_kengne@yahoo.com>
2023-04-25 23:26:59 -0400

    some modifs

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const res = require("express/lib/response");
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", function(req, res) {
     res.send('Hello World!');
 })
 
